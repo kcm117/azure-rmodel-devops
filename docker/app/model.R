e0705a1687d62748d0483f0a354a38af1ec4d4af
KC Munnings <kc@kcmunnings.com>
2019-05-22 11:32:21 -0400

    Test error handling in build process

--- docker/app/model.R
+++ docker/app/model.R
@@ -7,5 +7,9 @@ predict_weight <- function(height=0,model){
   a <- data.frame(x = as.numeric(height))
   result <-  predict(model,a)
   response <- list(response = result)
-  return(response)
+  
+  # Intentional Error
+  return(c(1,2,3,4,5,6,7,8,9,10))
+  # Correct Code
+  #return(response)
 }
