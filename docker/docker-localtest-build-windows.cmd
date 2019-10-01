6307d03d71e54cf48b0fea351c48222a5640f49d
KC Munnings <kcmunnin@microsoft.com>
2019-10-01 17:00:03 -0400

    updated comment

--- docker/docker-localtest-build-windows.cmd
+++ docker/docker-localtest-build-windows.cmd
@@ -4,5 +4,6 @@ copy "..\models\model.rds" ".\app"
 rem build the docker container
 docker build -t azure-rmodel-devops .
 
-rem Delete the model.rds file from the local /docker/app directory, we don't want it source controlled there.
+rem Delete the model.rds file from the local /docker/app directory
+rem We don't want a copy of our model file to be source controlled in another directory.
 del ".\app\model.rds"
